a3ae78fe091eb972f8cb38f517ab4f7a7f9dbac5
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-17 21:09:26 +0800

    part3: preparing to deploy to render

--- part3/index.js
+++ part3/index.js
@@ -1,119 +1,121 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors);
 
 const logPost = morgan.token("logPost", (req) => {
-    if (!req.method === "POST") {
-        return;
-    }
+  if (!req.method === "POST") {
+    return;
+  }
 
-    return JSON.stringify(req.body);
+  return JSON.stringify(req.body);
 });
 
 app.use(
-    morgan(
-        ":method :url :status :res[content-length] - :response-time ms :logPost",
-    ),
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :logPost",
+  ),
 );
 
 let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+  response.json(persons);
 });
 
 app.get("/info", (request, response) => {
-    const length = persons.length;
-    const currentDate = new Date();
-    const strout = `<p>Phonebook has info for ${length} people</p>${currentDate}`;
+  const length = persons.length;
+  const currentDate = new Date();
+  const strout = `<p>Phonebook has info for ${length} people</p>${currentDate}`;
 
-    response.send(strout);
+  response.send(strout);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    const person = persons.find((note) => note.id === id);
+  const id = request.params.id;
+  const person = persons.find((note) => note.id === id);
 
-    if (!person) {
-        response.status(404).json({ error: "content not found!" });
-        return;
-    }
+  if (!person) {
+    response.status(404).json({ error: "content not found!" });
+    return;
+  }
 
-    response.status(200).json(person);
+  response.status(200).json(person);
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    const originalLength = persons.length;
-    persons = persons.filter((person) => person.id !== id);
+  const id = request.params.id;
+  const originalLength = persons.length;
+  persons = persons.filter((person) => person.id !== id);
 
-    if (originalLength === persons.length) {
-        response.status(404).json({ message: "Person not found!" });
-        return;
-    }
+  if (originalLength === persons.length) {
+    response.status(404).json({ message: "Person not found!" });
+    return;
+  }
 
-    response.status(204).end();
+  response.status(204).end();
 });
 
 app.post("/api/persons", (request, response) => {
-    const body = request.body;
-    const name = body.name;
-    const number = body.number;
-    const id = Math.floor(Math.random() * 1_000_000).toString();
+  const body = request.body;
+  const name = body.name;
+  const number = body.number;
+  const id = Math.floor(Math.random() * 1_000_000).toString();
 
-    if (!name || !number) {
-        response.status(400).json({ error: "The name or number is missing!" });
-        return;
-    }
+  if (!name || !number) {
+    response.status(400).json({ error: "The name or number is missing!" });
+    return;
+  }
 
-    const exist = persons.find((person) => person.name === name);
+  const exist = persons.find((person) => person.name === name);
 
-    if (exist) {
-        response.status(400).json({ error: "name must be unique" });
-        return;
-    }
+  if (exist) {
+    response.status(400).json({ error: "name must be unique" });
+    return;
+  }
 
-    const newPerson = {
-        id: id,
-        name: name,
-        number: number,
-    };
+  const newPerson = {
+    id: id,
+    name: name,
+    number: number,
+  };
 
-    persons = persons.concat(newPerson);
+  persons = persons.concat(newPerson);
 
-    response.status(200).json(newPerson);
+  response.status(200).json(newPerson);
 });
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).json({ error: "unknown endpoint" });
+  response.status(404).json({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+  console.log(`Server listening on port: ${PORT}`);
 });
