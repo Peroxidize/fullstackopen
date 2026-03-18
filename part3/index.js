47d40cb98b997e917e8324c4bcf2c36c2d8f0cfd
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-18 19:06:44 +0800

    part3: exercise 3.13 & 3.14 completed

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const Phonebook = require("./models/phonebook");
 
 app.use(express.json());
 app.use(express.static("dist"));
@@ -19,31 +22,10 @@ app.use(
   ),
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Phonebook.find({}).then((phonebook) => {
+    response.json(phonebook);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -56,14 +38,9 @@ app.get("/info", (request, response) => {
 
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const person = persons.find((note) => note.id === id);
-
-  if (!person) {
-    response.status(404).json({ error: "content not found!" });
-    return;
-  }
-
-  response.status(200).json(person);
+  Phonebook.findById(id).then((phonebook) => {
+    response.json(phonebook);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -81,31 +58,28 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
   const name = body.name;
   const number = body.number;
-  const id = Math.floor(Math.random() * 1_000_000).toString();
 
   if (!name || !number) {
-    response.status(400).json({ error: "The name or number is missing!" });
-    return;
+    return response
+      .status(400)
+      .json({ error: "the name or number is missing!" });
   }
 
-  const exist = persons.find((person) => person.name === name);
-
-  if (exist) {
-    response.status(400).json({ error: "name must be unique" });
-    return;
-  }
-
-  const newPerson = {
-    id: id,
+  const phonebook = new Phonebook({
     name: name,
     number: number,
-  };
+  });
 
-  persons = persons.concat(newPerson);
-
-  response.status(200).json(newPerson);
+  phonebook.save().then((savedPhonebook) => {
+    response.json(savedPhonebook);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -114,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
