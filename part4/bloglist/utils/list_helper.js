3770c4c4ba68957f627dbebc6df3497b197cbafe
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-19 14:43:11 +0800

    part4: exercise 4.5 completed

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -18,7 +18,28 @@ const totalLikes = blogs => {
     return blogs.reduce(sum, 0);
 };
 
+const favoriteBlog = blogs => {
+    if (blogs.length === 1) {
+        return blogs[0];
+    }
+
+    if (blogs.length === 0) {
+        return blogs;
+    }
+
+    const mostLikes = (greatest, blog) => {
+        if (blog.likes > greatest.likes) {
+            return blog;
+        }
+
+        return greatest;
+    };
+
+    return blogs.reduce(mostLikes, blogs[0]);
+};
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
