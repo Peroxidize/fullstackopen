af72c5abcd2beac34f558aa7f3b243c6eea60473
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-19 19:31:05 +0800

    part4: exercise 4.10 completed

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,4 +1,5 @@
 const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
@@ -26,6 +27,25 @@ test("bloglist identifier is named id", async () => {
     await api.get("/api/blogs").expect(response => {
         response.body[0].id;
     }, sampleData.blogs[0]._id);
+});
+
+test("blog has been added to database successfully", async () => {
+    const sample = {
+        title: "STOP USING AI",
+        author: "Pewdiepie",
+        url: "https://youtube.com/",
+        likes: 69,
+    };
+
+    await api
+        .post("/api/blogs")
+        .send(sample)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+
+    assert.strictEqual(response.body.length, sampleData.blogs.length + 1);
 });
 
 after(async () => {
