a1524fe8d9653b2d3f49022ec400dc45e8d64bd2
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-22 16:10:59 +0800

    part5: exercise 5.7 completed

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (view) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}{" "}
+          <button onClick={() => setView(!view)}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setView(!view)}>view</button>
+    </div>
+  );
+};
 
 export default Blog;
