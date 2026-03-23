bdce2d7267ed6ef6cbc747dcdc5067658c0c7d14
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-23 15:19:49 +0800

    part5: exercise 5.14 completed

--- part5/bloglist-frontend/src/tests/Blog.test.jsx
+++ part5/bloglist-frontend/src/tests/Blog.test.jsx
@@ -1,16 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "../components/Blog";
 
 describe("Blog", () => {
-  test("does not render url or likes by default", () => {
-    const blog = {
-      title: "title test",
-      author: "pewdiepie",
-      url: "youtube.com",
-      likes: 123,
-      user: "1238789sdjkhf78123897",
-    };
+  const blog = {
+    title: "title test",
+    author: "pewdiepie",
+    url: "youtube.com",
+    likes: 123,
+    user: "1238789sdjkhf78123897",
+  };
 
+  test("does not render url or likes by default", () => {
     render(<Blog blog={blog} />);
 
     const element = screen.queryByText(`${blog.title} ${blog.author}`);
@@ -20,5 +21,22 @@ describe("Blog", () => {
     expect(element).toBeDefined();
     expect(element2).toBeNull();
     expect(element3).toBeNull();
+  });
+
+  test("url and likes are shown when button is clicked", async () => {
+    render(<Blog blog={blog} />);
+
+    const user = userEvent.setup();
+
+    const button = screen.queryByText("view");
+    await user.click(button);
+
+    const element = screen.queryByText(`${blog.title} ${blog.author}`);
+    const element2 = screen.queryByText(`${blog.url}`);
+    const element3 = screen.queryByText(`${blog.likes}`);
+
+    expect(element).toBeDefined();
+    expect(element2).toBeDefined();
+    expect(element3).toBeDefined();
   });
 });
