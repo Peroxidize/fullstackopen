56b4c488b924075c591d72b0e14c1819e1166b92
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-23 16:26:24 +0800

    refactor: exercise 5.15

--- part5/bloglist-frontend/src/tests/Blog.test.jsx
+++ part5/bloglist-frontend/src/tests/Blog.test.jsx
@@ -1,16 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Blog from "../components/Blog";
+import blogService from "../services/blogs";
+
+vi.mock("../services/blog");
+
+const blog = {
+  id: "1",
+  title: "title test",
+  author: "pewdiepie",
+  url: "youtube.com",
+  likes: 123,
+  user: "1238789sdjkhf78123897",
+};
 
 describe("Blog", () => {
-  const blog = {
-    title: "title test",
-    author: "pewdiepie",
-    url: "youtube.com",
-    likes: 123,
-    user: "1238789sdjkhf78123897",
-  };
-
   test("does not render url or likes by default", () => {
     render(<Blog blog={blog} />);
 
@@ -24,11 +28,27 @@ describe("Blog", () => {
   });
 
   test("url and likes are shown when button is clicked", async () => {
+    render(<Blog blog={blog} />);
+
+    const element = screen.queryByText(`${blog.title} ${blog.author}`);
+    const element2 = screen.queryByText(`${blog.url}`);
+    const element3 = screen.queryByText(`${blog.likes}`);
+
+    expect(element).toBeDefined();
+    expect(element2).toBeDefined();
+    expect(element3).toBeDefined();
+  });
+
+  test("if like button is clicked twice the event handler is also called twice", async () => {
     const user = userEvent.setup();
-    const mockHandler = vi.fn();
+    const fetchBlogs = vi.fn();
+    const showMessage = vi.fn();
+    const increaseLikesMock = vi
+      .spyOn(blogService, "increaseLikes")
+      .mockResolvedValue({});
 
     render(
-      <Blog blog={blog} fetchBlogs={mockHandler} showMessage={mockHandler} />
+      <Blog blog={blog} fetchBlogs={fetchBlogs} showMessage={showMessage} />
     );
 
     const viewButton = screen.queryByText("view");
@@ -38,6 +58,10 @@ describe("Blog", () => {
     await user.click(likeButton);
     await user.click(likeButton);
 
-    expect(mockHandler.mock.calls).toHaveLength(2);
+    console.log(increaseLikesMock);
+    expect(increaseLikesMock).toHaveBeenCalledTimes(2);
+    expect(increaseLikesMock).toHaveBeenCalledWith(blog.id);
+
+    increaseLikesMock.mockRestore();
   });
 });
