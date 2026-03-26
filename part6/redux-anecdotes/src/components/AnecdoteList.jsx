c979a58bd637de0214e474ca3033c6618947fd62
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-26 16:07:53 +0800

    part6: exercise 6.8 completed

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increaseVote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector(state => state);
+
+  const vote = id => {
+    console.log("vote", id);
+    dispatch(increaseVote(id));
+  };
+
+  return (
+    <>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default AnecdoteList;
