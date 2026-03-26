b85d837508bf7e3f0d782468101f019b2bf4cf44
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-26 17:52:18 +0800

    part6: exercise 6.9 completed

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,12 @@ import { increaseVote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
   const dispatch = useDispatch();
-  const anecdotes = useSelector(state => state);
+  const anecdotes = useSelector(state => {
+    const filter = state.filter;
+    return state.anecdotes.filter(anecdote =>
+      anecdote.content.includes(filter)
+    );
+  });
 
   const vote = id => {
     console.log("vote", id);
