eac83cb97ea9a55abb396ba5a5b8591e9b4204ea
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-27 10:34:37 +0800

    part6: exercise 6.15 completed

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = anecdote => {
-    return {
-        content: anecdote,
-        id: getId(),
-        votes: 0,
-    };
-};
+// const getId = () => (100000 * Math.random()).toFixed(0);
+//
+// const asObject = anecdote => {
+//     return {
+//         content: anecdote,
+//         id: getId(),
+//         votes: 0,
+//     };
+// };
 
 const initialState = [];
 
@@ -17,7 +17,7 @@ const anecdoteSlice = createSlice({
     initialState,
     reducers: {
         createAnecdote(state, action) {
-            state.push(asObject(action.payload));
+            state.push(action.payload);
         },
         increaseVote(state, action) {
             const id = action.payload;
